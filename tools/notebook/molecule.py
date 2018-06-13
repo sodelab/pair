1b3a96af5d92bfca2d97ec722036093dc0348d64
oosode <olaseni.sode@gmail.com>
2018-06-13 10:57:30 -0400

    Ahead of parsl demo.

--- tools/notebook/molecule.py
+++ tools/notebook/molecule.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python 
 
-import math,os,sys
+import os,sys
 import cirpy
 import element
+from math import *
+from copy import *
 
 class molecule:
     def __init__(self,name,nmols):
@@ -13,14 +15,11 @@ class molecule:
         self.mass = []
         self.atom = []
         self.frag = []
-        print(self.name)
 
         return
 
     def resolve(self):
-        print(self.name)
-        #return
-        res=cirpy.resolve('%s'%(self.name), 'xyz')
+        res=cirpy.resolve(self.name, 'xyz')
         print("Resolved chemical identification of %s"%(self.name))
         r=res.split("\n")
         self.natoms=int(r[0])
@@ -35,26 +34,49 @@ class molecule:
                      float(line.split()[3])]
                 self.coord.append(tmp)
                 self.atom.append(line.split()[0])
-                self.mass.append(ELEMENT[self.atom[-1].mass])
+                self.mass.append(element.ELEMENTS[self.atom[-1]].mass)
             else:
                 continue
         return
     
     def init_dimer(self,m1,m2):
         
+        
         self.name=m1.name+"-"+m2.name
         self.nmols=2
-        self.coord=m1.coord+m2.coord
-        self.mass=m1.mass+m2.mass
+        self.coord=deepcopy(m1.coord)+deepcopy(m2.coord)
+        self.mass=deepcopy(m1.mass)+deepcopy(m2.mass)
         self.natoms=m1.natoms+m2.natoms
-        self.atom=m1.atom+m2.atom
+        self.atom=deepcopy(m1.atom)+deepcopy(m2.atom)
         self.frag=[0]*m1.natoms+[1]*m2.natoms
 
         return
+    
+    def xyz(self):
         
+        string="%d\n\n"%(self.natoms)
+        for c,coord in enumerate(self.coord):
+            string+="%-2s "%(self.atom[c])
+            string+="%15.10f %15.10f %15.10f\n"%(coord[0],
+                                                 coord[1],
+                                                 coord[2])
+            
+        return string
+        
+    def print_xyz(self):
+        
+        string="%d\n\n"%(self.natoms)
+        for c,coord in enumerate(self.coord):
+            string+="%-2s "%(self.atom[c])
+            string+="%15.10f %15.10f %15.10f\n"%(coord[0],
+                                                 coord[1],
+                                                 coord[2])
+        print(string)
+        return
+    
     def calculate_com(self):
     
-        if nmols==1:
+        if self.nmols==1:
             x=0.0
             y=0.0
             z=0.0
@@ -74,25 +96,25 @@ class molecule:
 
             self.com=[x,y,z]
 
-        elif nmols==2:
+        elif self.nmols==2:
 
-            x1,x2=0.0
-            y1,y2=0.0
-            z1,z2=0.0
+            x1=x2=0.0
+            y1=y2=0.0
+            z1=z2=0.0
 
-            totalmass1,totalmass2=0.0
+            totalmass1=totalmass2=0.0
 
             for c,coord in enumerate(self.coord):
 
-                if self.frag[j]==0:
-                    x1+=atom[0]*self.mass[c]
-                    y1+=atom[1]*self.mass[c]
-                    z1+=atom[2]*self.mass[c]
+                if self.frag[c]==0:
+                    x1+=coord[0]*self.mass[c]
+                    y1+=coord[1]*self.mass[c]
+                    z1+=coord[2]*self.mass[c]
                     totalmass1+=self.mass[c]
-                elif self.frag[j]==1:
-                    x2+=atom[0]*self.mass[c]
-                    y2+=atom[1]*self.mass[c]
-                    z2+=atom[2]*self.mass[c]
+                elif self.frag[c]==1:
+                    x2+=coord[0]*self.mass[c]
+                    y2+=coord[1]*self.mass[c]
+                    z2+=coord[2]*self.mass[c]
                     totalmass2+=self.mass[c]
 
             x1=x1/totalmass1
@@ -107,12 +129,29 @@ class molecule:
                    
         return
    
+    def calculate_vector(self):
+        """Function to calculate normalized vector."""
+
+        self.calculate_com()
+        
+        for l in range(3):
+            x=self.com[3]-self.com[0]
+            y=self.com[4]-self.com[1]
+            z=self.com[5]-self.com[2]
+            
+        norm=sqrt(x*x + y*y + z*z)
+        self.vector=[x/norm,y/norm,z/norm]
+        self.radius=norm
+        
+        return
+
+    
     def center(self):
 
         self.calculate_com()
         f=self.frag
                    
-        for c,coord in self.coord:
+        for c,coord in enumerate(self.coord):
             coord[0]-=self.com[0+f[c]*3]
             coord[1]-=self.com[1+f[c]*3]
             coord[2]-=self.com[2+f[c]*3]         
@@ -124,7 +163,7 @@ class molecule:
         if mID!=0 and self.nmols==1:
             print("This is a single molecule. The molecule ID should be zero.")  
             
-        for c,coord in self.coord:
+        for c,coord in enumerate(self.coord):
             
             if self.frag[c]==mID:
                 x=coord[0]
@@ -150,7 +189,7 @@ class molecule:
         if mID!=0 and self.nmols==1:
             print("This is a single molecule. The molecule ID should be zero.")  
             
-        for c,coord in self.coord:
+        for c,coord in enumerate(self.coord):
             
             if self.frag[c]==mID:
                 x=coord[0]
@@ -176,11 +215,11 @@ class molecule:
         if self.nmols==1:
             print("This is a single molecule. The `position_molecule` function is for dimers.")        
 
-        x=R*sin(theta)*cos(phi)
-        y=R*sin(theta)*sin(phi)
-        z=R*cos(theta)
+        x=R*sin(radians(theta))*cos(radians(phi))
+        y=R*sin(radians(theta))*sin(radians(phi))
+        z=R*cos(radians(theta))
 
-        for c,coord in self.coord:
+        for c,coord in enumerate(self.coord):
             
             if self.frag[c]==1:
                 coord[0]+=x
@@ -194,22 +233,22 @@ class molecule:
         transformation."""
         
         self.calculate_com()
-        self.R=0.0 # initial dimer distance
-        self.tv=[0.0]*3 # initial dimer vector
+        #self.R=0.0 # initial dimer distance
+        #self.tv=[0.0]*3 # initial dimer vector
         self.center()
         return
         
-    def transform_dimer(self,value,transformation,mID):
+    def transform_dimer(self,value,transform,mID):
         """This function prepares the dimer according to the type and magnitude 
         of transformation. Since translations shift the dimer off the origin, this
         operation is always perfomed last in the `set dimer` function."""
         
         if transform=="translation": # make translational vector
-            self.R+=value
+            self.radius=value
             
         elif transform=="phi" or transform=="theta": # rotate molecules
             
-            for c,coord in self.coord:
+            for c,coord in enumerate(self.coord):
             
                 if self.frag[c]==mID:
                     x=coord[0]
@@ -244,11 +283,11 @@ class molecule:
     def set_dimer(self):
         """"""
         
-        for c,coord in self.coord:
+        for c,coord in enumerate(self.coord):
             if self.frag[c]==1:
-                coord[0]+=self.tv[0]*self.R
-                coord[1]+=self.tv[1]*self.R
-                coord[2]+=self.tv[2]*self.R
+                coord[0]+=self.vector[0]*self.radius
+                coord[1]+=self.vector[1]*self.radius
+                coord[2]+=self.vector[2]*self.radius
                 
         return
 
